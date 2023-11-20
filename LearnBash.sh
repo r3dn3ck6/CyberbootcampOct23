a66b51e29d7132e2317ca93ee039a69a1f310ddb
Josh Hughes <n8tivet3xn@gmail.com>
2023-11-20 17:29:10 -0600

    updates

--- LearnBash.sh
+++ LearnBash.sh
@@ -51,27 +51,30 @@
 # #     then echo "Number is 5"
 # # fi
 
-#While loop run till acceptance not met
 user=y
 while [ $user = y ]
-do
-echo "Please make a selection"
-echo "1 to print hello world" 
-echo "2 ping yourself"
-echo "3 to show network config"
-read input
-    if [ $input = 1 ]
-        then echo "Hello World!"
-    elif [ $input = 2 ]
-        then ping -c 1 8.8.8.8
-    elif [ $input = 3 ]
-        then ifconfig
-    else echo "Invalid entry"
-fi
-echo "do you want to play again: y/n"
-read user
-done
-echo "good work"
+ do
+ echo "Please make a selection"
+ echo "1 to print hello world" 
+ echo "2 ping a website or ip address"
+ echo "3 to show network config"
+ read input
+     if [ $input = 1 ]
+         then echo "Hello World!"
+     elif [ $input = 2 ]
+         then
+            echo "Enter website or ip address to ping:"
+            read address
+            ping -c 5 $address
+     elif [ $input = 3 ]
+         then ifconfig
+     else 
+       echo "Invalid entry"
+ fi
+ echo "do you want to play again: y/n"
+ read user
+ done
+ echo "good work"
 
 # #functions is to be called multiple times by only using function name
 # echo "Enter 2 numbers to add"
