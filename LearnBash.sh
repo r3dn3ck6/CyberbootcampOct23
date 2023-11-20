4e98b22bd983f942cf88aecfa428ae8b31b2efab
Josh Hughes <n8tivet3xn@gmail.com>
2023-11-20 17:25:43 -0600

    updates

--- LearnBash.sh
+++ LearnBash.sh
@@ -51,37 +51,37 @@
 # #     then echo "Number is 5"
 # # fi
 
-# #While loop run till acceptance not met
-# user=y
-# while [ $user = y ]
-# do
-# echo "Please make a selection"
-# echo "1 to print hello world" 
-# echo "2 ping yourself"
-# echo "3 to show network config"
-# read input
-#     if [ $input = 1 ]
-#         then echo "Hello World!"
-#     elif [ $input = 2 ]
-#         then ping -c 1 192.168.0.1
-#     elif [ $input = 3 ]
-#         then ifconfig
-#     else echo "Invalid entry"
-# fi
-# echo "do you want to play again: y/n"
-# read user
-# done
-# echo "good work"
+#While loop run till acceptance not met
+user=y
+while [ $user = y ]
+do
+echo "Please make a selection"
+echo "1 to print hello world" 
+echo "2 ping yourself"
+echo "3 to show network config"
+read input
+    if [ $input = 1 ]
+        then echo "Hello World!"
+    elif [ $input = 2 ]
+        then ping -c 1 8.8.8.8
+    elif [ $input = 3 ]
+        then ifconfig
+    else echo "Invalid entry"
+fi
+echo "do you want to play again: y/n"
+read user
+done
+echo "good work"
 
 # #functions is to be called multiple times by only using function name
-echo "Enter 2 numbers to add"
-read num1
-read num2
-function add(){
-    sum=$(( $num1 + $num2 ))
-    echo $sum
+# echo "Enter 2 numbers to add"
+# read num1
+# read num2
+# function add(){
+#     sum=$(( $num1 + $num2 ))
+#     echo $sum
 
-}
+# }
 # add
 
 # x=1
