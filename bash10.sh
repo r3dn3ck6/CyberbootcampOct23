361968fe3492e578488a828e1610b35f076d92f6
Josh Hughes <n8tivet3xn@gmail.com>
2023-11-21 12:18:57 -0600

    updates

--- bash10.sh
+++ bash10.sh
@@ -9,3 +9,6 @@ do
     echo "x is $x"
     ((x++))
 done
+
+This script initializes a variable x with a value of 0 and runs an until loop until the value of x is greater than 10. 
+Inside the loop, it echoes the current value of x and then increments it by 1 using ((x++)).
