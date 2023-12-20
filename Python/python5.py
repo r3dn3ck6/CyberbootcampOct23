15c265cc921b45c303969be19a439acf3e3b8077
Josh Hughes <n8tivet3xn@gmail.com>
2023-12-19 18:32:27 -0600

    updates

---
+++ Python/python5.py
@@ -0,0 +1,34 @@
+# Write a program that works out whether if a given number is an 
+# odd or even number
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
+
+# Create a program using maths and f-Strings that tells us how many 
+# days, weeks, months we have left if we live until 90 years old.
+
+# It will take your current age as the input and output a message 
+# with our time left in this format:
+
+# You have x days, y weeks, and z months left.
+
+# Where x, y and z are replaced with the actual calculated numbers.
+
+
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+years_remaining = 90 - age
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left until 90 years old.")
